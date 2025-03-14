db0f1b60b6c7d386ac04f5c2b6ecdb090093b82b
Kaseiinov_I <izlax6002@gmail.com>
2025-03-14 16:13:18 +0600

    Made class User

---
+++ src/main/java/com/example/home_work_49/models/User.java
@@ -0,0 +1,13 @@
+package com.example.home_work_49.models;
+
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String avatar;
+    private String accountType;
+}
