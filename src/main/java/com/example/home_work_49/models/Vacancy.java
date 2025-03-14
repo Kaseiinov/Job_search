e6d6cbe99ad46d02ec41c2074f4fabb9bf04b5c9
Kaseiinov_I <izlax6002@gmail.com>
2025-03-14 16:14:11 +0600

    Made class Vacancy

---
+++ src/main/java/com/example/home_work_49/models/Vacancy.java
@@ -0,0 +1,15 @@
+package com.example.home_work_49.models;
+
+public class Vacancy {
+    private int id;
+    private String name;
+    private String description;
+    private Integer categoryId;
+    private double salary;
+    private int expFrom;
+    private int expTo;
+    private boolean isActive;
+    private int authorId;
+    private String createdDate;
+    private String updateTime;
+}
