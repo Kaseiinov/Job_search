4b42e6f0b151cc06785c2bad6db291427cd6e984
Kaseiinov_I <izlax6002@gmail.com>
2025-05-20 18:47:38 +0600

    Added js function for show input if the button will be clicked

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,2 +1,25 @@
+$(document).ready(function (){
+    const aboutMeCard = $('.about_me');
+    const educationCard = $('.education');
+    const contactCard = $('.contacts');
 
+    aboutMeCard.hide();
+    educationCard.hide();
+    contactCard.hide();
+
+    let buttonAboutMe = $('#aboutMe');
+    buttonAboutMe.click(() => {
+        aboutMeCard.show();
+    })
+
+    let buttonEducation = $('#educationButton');
+    buttonEducation.click(() => {
+        educationCard.show();
+    })
+
+    let buttonContact = $('#contactButton');
+    buttonContact.click(() => {
+        contactCard.show();
+    })
+})
 
